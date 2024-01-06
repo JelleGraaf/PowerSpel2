145c05641fc47106cf2ac5200e08b0939c313121
jellegraaf <palagast@hotmail.com>
2024-01-06 09:34:30 +0100

    Expand CBH in New-Move

--- Helpers/New-Move.ps1
+++ Helpers/New-Move.ps1
@@ -3,11 +3,19 @@ function New-Move {
     .SYNOPSIS
         Moves the character to a different room.
 
+    .DESCRIPTION
+        Moves the character to a different room, if the move is valid (both a room must be present and a wall absent, in the given direction).
+
     .PARAMETER Direction
         Direction of the room to move to, relative to the current room.
 
     .EXAMPLE
         ps> New-Move -Direction E
+
+    .NOTES
+        Moving works by adjusting the $State.CurrentRoom value. The room coordinates consist of six numbers. The first two numbers are for
+        the x-axis (east-west), the second pair for the y-axis (north-south) and the third pair for the z-axis (up-down). This means that
+        moving one room up, increases the $State.CurrentRoom variable by one. Moving two rooms south, decreases this variable by 200, etc.
     #>
 
     param(
