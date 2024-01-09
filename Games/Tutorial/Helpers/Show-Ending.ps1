d8455112310e195265e90e0489f781ce99c14f83
jellegraaf <palagast@hotmail.com>
2024-01-09 21:39:42 +0100

    Add some dummy ending material

--- Games/Tutorial/Helpers/Show-Ending.ps1
+++ Games/Tutorial/Helpers/Show-Ending.ps1
@@ -28,7 +28,26 @@ function Show-Ending {
     Write-Host "                                 ███    ███   ███    ███    ▄█    ███ ███    ███ ███▌    ▄     ███    " -ForegroundColor White
     Write-Host "                                 ███    ███   ██████████  ▄████████▀  ████████▀  █████▄▄██    ▄████▀  " -ForegroundColor White
     Write-Host "                                 ███    ███                                      ▀                    " -ForegroundColor White
+    Write-Host
 
 
     # Some if statements here. Show things in green for what the played has accomplished and in red for what was skipped.
+    Start-Sleep 1
+    if ($State.Achievements -contains "Attic explorer") {
+        Write-Host "You have visted the attic. Seen any ghosts?" -ForegroundColor Green
+    }
+    else {
+        Write-Host "You did not visit the attic. Are you sure you are OK with that?" -ForegroundColor Red
+    }
+    Start-Sleep 1
+
+    if ($State.Achievements -contains "Collector") {
+        Write-Host "You have collected both the mug and the pen. You hoarder, you!" -ForegroundColor Green
+    }
+    else {
+        Write-Host "You did get all available items. Next time, look closer for pick-ups!" -ForegroundColor Red
+    }
+    Start-Sleep 1
+
+    Write-Host "Einde" -ForegroundColor Green
 }
