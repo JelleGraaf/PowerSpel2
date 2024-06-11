72cdb2fd8a2eca4638dae25efb3e2b262ba2673a
jellegraaf <palagast@hotmail.com>
2024-06-11 20:09:50 +0200

    Add ending check function to tutorial game

---
+++ Helpers/Invoke-EndingCheck.ps1
@@ -0,0 +1,9 @@
+function Invoke-EndingCheck {
+    <#
+    .SYNOPSIS
+        Checks for conditions of certain ending scenarios and ends the game if those conditions are met.
+    #>
+
+    # Check for game endings.
+    # In the tutorial, there are no alternative endings to check for.
+}
